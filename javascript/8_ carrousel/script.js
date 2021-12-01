b1ba4a963da7272b42f422684cd88fa46978bed9
tristangraaff <tristangraaff@yahoo.com>
2021-12-01 16:36:42 +0100

    sliding images bug fix

--- javascript/8_ carrousel/script.js
+++ javascript/8_ carrousel/script.js
@@ -7,41 +7,74 @@ let slideIndex = 1;
 const showImages = (n) => {
   if (n < 1) {slideIndex = images.length}
   if (n > images.length) {slideIndex = 1}
-  for (let item of images) {
-     item.style.display = "none";
-  }
-  console.log(slideIndex);
-  console.log(images[slideIndex - 1]);
+  console.log("IMAGE DISPLAY IS CHANGED TO BLOCK" , `SLIDEINDEX = ${slideIndex}`)
   images[slideIndex - 1].style.display = "block";
 }
 
-const showFirstTime = () => {
-  images[slideIndex - 1].style.right = "0";
+const showStartupImg = () => {
+  console.log("FIRST IMAGE IS RENDERED WITHIN SCREEN SIZE ON STARTUP")
+  images[0].style.right = "0";
+}
+
+const moveStartupImgAway = () => {
+  if (images[0].style.right == "0px") {
+    console.log("FIRST IMAGE IS POSITIONED OUTSIDE OF SCREEN")
+    images[0].style.right = "100%";
+  }
+}
+
+const keepShowingFormerImgOnScreen = () => {
+  if (slideIndex === images.length) {
+    console.log("FIRST IMG IS POSITIONED INSIDE OF SCREEN")
+    images[0].style.right = "0";
+  } else {
+    console.log("IMG BEFORE CURRENT IS POSITIONED INSIDE OF SCREEN")
+    images[slideIndex].style.right = "0";
+  }
+}
+
+const slideNewImgIntoScreen = () => {
+  setTimeout(() => {
+    if(slideIndex === 1) {
+      console.log("LAST IMG SLIDES INTO SCREEN")
+      images[0].classList.add("slidingImg"); 
+      console.log(images[0]);
+    } else {
+      console.log("CURRENT IMG SLIDES INTO SCREEN")
+      images[slideIndex - 1].classList.add("slidingImg");
+      console.log(images[slideIndex - 1]);   
+    }
+  });
+}
+
+const removeFormerImgFromScreen = () => {
+  setTimeout(() => {
+    if (slideIndex === images.length) {
+      console.log("FIRST IMG IS POSITIONED OUTSIDE OF SCREEN")
+      images[0].style.right = "100%";
+    } else {
+      console.log("IMG BEFORE CURRENT IS POSITIONED OUTSIDE OF SCREEN")
+      images[slideIndex].style.right = "100%";
+    }
+  }, 2000);
 }
 
 const moveImages = (n) => {
+  console.log("SHOWN IMAGES IS RUN, SLIDEINDEX DIMINISHES BY 1")
   slideIndex += n;
   showImages(slideIndex);
 }
 
 const slideImages = () => {
+  moveStartupImgAway();
   for (let item of images) {
+    console.log("ALL SLIDING IMG CLASSES ARE REMOVED")
     item.classList.remove("slidingImg");
-    // item.style.right = "100%";
   }
-  // console.log(images[slideIndex]);
-  // images[slideIndex].style.right = "0";
-  console.log(images[slideIndex - 1]);
-  setTimeout(() => {
-    images[slideIndex - 1].classList.add("slidingImg");
-  });
-  setTimeout(() => {
-    images[slideIndex].style.right = "100%";
-  }, 2000);
+  keepShowingFormerImgOnScreen();
+  slideNewImgIntoScreen();
+  removeFormerImgFromScreen();
 }
-
-showImages(slideIndex);
-showFirstTime();
 
 leftArrow.addEventListener("click", () => {
   moveImages(-1);
@@ -50,19 +83,8 @@ leftArrow.addEventListener("click", () => {
 
 rightArrow.addEventListener("click", () => {
   moveImages(1);
-  // slideImages();
+  slideImages();
 })
 
-
-// const movePictures = () => {
-//   for (let item of images) {
-//     const computedStyles = getComputedStyle(item);
-//     const leftPositionNumber = computedStyles.left.replace("px", "");
-//     const leftPositionInt = parseInt(leftPositionNumber);
-//     console.log(leftPositionInt);
-//     const newPosition = leftPositionInt - 1000;
-//     const newPositionString = newPosition.toString();
-//     console.log(newPositionString);
-//     item.style.left = newPositionString;
-//   }
-// }
+showStartupImg();
+showImages(slideIndex);
